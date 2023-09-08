05ca6fb63c393c18e88e925cdcbaf1e7a89b81de
liShuangQ-mac <li_shuangq@foxmail.com>
2023-09-08 16:59:29 +0800

    tailwind动态配置 vueuse例子

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -83,7 +83,7 @@ module.exports = {
     },
     output: {
         filename: './js/[name].js',
-        path: path.resolve('/wv3', ''),
+        path: path.resolve(__dirname, '../dist'),
         publicPath: devMode ? '/' : '/wv3/'
     }
 };
