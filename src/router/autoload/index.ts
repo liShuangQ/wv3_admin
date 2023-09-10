fa4723fd1a0028ed7801008f8c3c4f7e226d8f0d
liShuangQ <li_shuangq@foxmail.com>
2023-09-10 14:14:30 +0800

    Test (#2)
    * 引入element
    
    * 优化路由

--- src/router/autoload/index.ts
+++ src/router/autoload/index.ts
@@ -1,56 +1,56 @@
-import { RouteRecordRaw } from "vue-router";
-const layoutsFun = (require as any).context('../../layouts', false, /\.vue$/,'sync');
-let layouts:{} = {}
-layoutsFun.keys().forEach((k:string )=>{
-  layouts[k] = layoutsFun(k)
+import {RouteRecordRaw} from "vue-router";
+
+const layoutsFun = (require as any).context('../../layouts', false, /\.vue$/, 'sync');
+let layouts: {} = {}
+layoutsFun.keys().forEach((k: string) => {
+    layouts[k] = layoutsFun(k)
 })
 
-const viewsFun = (require as any).context('../../views', true, /\index.vue$/,'sync');
-let views:{} = {}
-viewsFun.keys().forEach((k:string )=>{
+const viewsFun = (require as any).context('../../views', true, /\index.vue$/, 'sync');
+let views: {} = {}
+viewsFun.keys().forEach((k: string) => {
     views[k] = viewsFun(k)
 })
 
 
 function getRoutes() {
-  const layoutRoutes = [] as RouteRecordRaw[];
-  Object.entries(layouts).forEach(([file, module]) => {
-    const route = getRouteByModule(file, module as any);
-    
-    route["children"] = getChildrenRoutes(route);
-    layoutRoutes.push(route);
-  });
-  return layoutRoutes;
+    const layoutRoutes = [] as RouteRecordRaw[];
+    Object.entries(layouts).forEach(([file, module]) => {
+        const route = getRouteByModule(file, module as any, '1');
+
+        route["children"] = getChildrenRoutes(route);
+        layoutRoutes.push(route);
+    });
+    return layoutRoutes;
 }
 
 function getChildrenRoutes(layoutRoute: RouteRecordRaw) {
-  const routes = [] as RouteRecordRaw[];
-  Object.entries(views).forEach(([file, module]) => {
-    
-    if (file.includes(`${layoutRoute.name as string}`)) {
-      const route = getRouteByModule(file, module as any);
-      routes.push(route);
-    }
-  });
-  
-  return routes;
+    const routes = [] as RouteRecordRaw[];
+    Object.entries(views).forEach(([file, module]) => {
+
+        if (file.includes(`${layoutRoute.name as string}`)) {
+            const route = getRouteByModule(file, module as any, '2');
+            routes.push(route);
+        }
+    });
+
+    return routes;
 }
 
-function getRouteByModule(file: string, module: { [key: string]: any }) {
-  const path = file.replace(/^\.|\/index|\.vue/gi, "");
-  
-  const router = {
-    name: path.replace(/^\//, '').replace(/\//g, '.'),
-    path: path,
-    component: module.default,
-    meta:{}
-  } as RouteRecordRaw;
+function getRouteByModule(file: string, module: { [key: string]: any }, type: string) {
+    const path = file.replace(/^\.|\/index|\.vue/gi, "");
+    const router = {
+        name: path.replace(/^\//, '').replace(/\//g, '.'),
+        path: path,
+        component: type === '1' ? module.default :() => import(`../../views${path}/index.vue`),
+        meta: {}
+    } as RouteRecordRaw;
 
-  //自定义路由: export default {
-  //   route: { path: "/user" },
-  // };
-  return Object.assign(router, module.default?.route);
-  
+    //自定义路由: export default {
+    //   route: { path: "/user" },
+    // };
+    return Object.assign(router, module.default?.route);
+
 }
 
 const routes = getRoutes()
