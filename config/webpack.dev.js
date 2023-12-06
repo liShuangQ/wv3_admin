eaa9ad5c138015e04d22eae0c0dcf989db71a601
liShuangQ-mac <li_shuangq@foxmail.com>
2023-12-06 18:04:16 +0800

    webpack配置添加

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,21 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const baseConfig = require("./webpack.base.js");
-const { merge } = require("webpack-merge");
+const {merge} = require("webpack-merge");
 const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 module.exports = merge(baseConfig, {
     mode: "development",
     target: "web",
     devServer: {
+        port: '8077',
         hot: true,
-        open: true,
+        open: false,
         client: {
             overlay: false,
+        },
+        proxy: {
+            // 配置代理，解决开发环境的跨域问题
+            // '/api': 'http://localhost:3000'
         },
     },
     devtool: false,//eval-cheap-module-source-map
     plugins: [
         new FriendlyErrorsWebpackPlugin(),
-        new ESLintPlugin({ extensions: ["js", "ts", "vue"] }),
+        new ESLintPlugin({extensions: ["js", "ts", "vue"]}),
     ],
 });
