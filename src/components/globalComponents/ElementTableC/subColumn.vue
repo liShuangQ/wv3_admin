3e4eb1c8197132965a4c31263d6b0c664212aabf
liShuangQ-mac <li_shuangq@foxmail.com>
2024-03-01 10:49:14 +0800

    表格添加link

--- src/components/globalComponents/ElementTableC/subColumn.vue
+++ src/components/globalComponents/ElementTableC/subColumn.vue
@@ -18,18 +18,26 @@
                 v-for="item in columnConfig.children"
                 :key="item.prop"
                 :columnConfig="item"
+                :faEmit="props.faEmit"
                 :tooltip="props.tooltip"
             >
             </sub-column>
         </template>
-
+        <template v-if="columnConfig.isLink" #default="scope">
+            <el-button
+                :type="columnConfig.isLink || 'primary'"
+                link
+                @click="faEmit('handle','link',toRaw(scope.row),columnConfig.prop)"
+            >{{ scope.row[columnConfig.prop] }}
+            </el-button>
+        </template>
         <!-- XXX: 可能会小概率引发和自身的data变量的key冲突 -->
         <template v-if="columnConfig.isEdit" #default="scope">
             <el-input
                 v-if="scope.row[columnConfig.prop + 'Edit']"
                 v-model="scope.row[columnConfig.prop]"
-                placeholder=""
                 :size="'default'"
+                placeholder=""
             />
             <span v-else>{{ scope.row[columnConfig.prop] }}</span>
         </template>
@@ -37,12 +45,16 @@
 </template>
 
 <script lang="ts" setup>
-import { TableColumnConfig } from "./table-component";
+import {TableColumnConfig} from "./table-component";
+import {toRaw} from 'vue'
 
 const props = defineProps<{
     columnConfig: TableColumnConfig;
     tooltip?: boolean;
+    faEmit?: any
 }>();
+
+
 </script>
 
 <style scoped></style>
